0cc10ecef86eff0afba2a8de5213eab29d17d7e2
PRAKASH ANAND <34940322+GHATAK123@users.noreply.github.com>
2021-07-15 11:19:10 +0530

    Getting Started with Vue

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product : "Socks",
+            description: "It is used with shoes",
+            image : "./assets/images/socks_green.jpg",
+            url : "http://facebbook.com",
+            inverntory : 1000,
+            onSale : true,
+            details : ["40% Cotton","30% wool","30% Polyester"],
+            variants : [
+                {id:1,color:"green"},
+                {id:2,color:"blue"}
+            ]
+        }
+    },
+})
